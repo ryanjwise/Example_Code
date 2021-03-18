2fac8938a2769405bec5718c685f2afee92c2332
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-18 15:08:03 +1100

    Added additional array method example

--- ruby/Methods/Array methods.rb
+++ ruby/Methods/Array methods.rb
@@ -1,7 +1,10 @@
 arr = [1,2,3,4,5,6]
+arr2 = [7,8,9,10,11]
 
-arr.length      #retruns length od array
-arr.join        #returns array as one continuous element "123456"
-arr.include?()  #Searches for passed element and returns true or false
-arr.delete()    # deletes any antries with a matcing value 
-arr.delete_at() # Delete entry at passed index
+arr.length          # retruns length od array
+arr.join            # returns array as one continuous element "123456"
+arr.include?()      # Searches for passed element and returns true or false
+arr.delete()        # deletes any antries with a matcing value 
+arr.delete_at()     # Delete entry at passed index
+arr.flatten!        # Will flatten a multidimensional array
+arr3 = arr + arr2   # Will add the two arrays together [1,2,3,4,5,6,7,8,9,10,11]]
