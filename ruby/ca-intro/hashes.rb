2c5a0ee2a5937f9f943729c2fb522a0768fabfe8
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-18 15:07:29 +1100

    Commented ca-hashes intro

--- ruby/ca-intro/hashes.rb
+++ ruby/ca-intro/hashes.rb
@@ -2,9 +2,12 @@
 # Similar to a dictionary file
 # There is a key value pair for each item.
 
+
+## Create Hash
 hash = {}
 {}
 
+## Create hash with value
 hash = {name: 'daniel'}
 {:name=>"daniel"}
 
@@ -12,25 +15,38 @@ hash = {name: 'daniel'}
 hash = {name: 'Harrison', age: 27, hobby: ['coding', 'coffee', 'golf','zelda']}
 {:name=>"Harrison", :age=>27, :hobby=>["coding", "coffee", "golf", "zelda"]}
 
+## Call a  hash value
 hash[:hobby]
 ["coding", "coffee", "golf", "zelda"]
 
+### Call an array item from a hash value
 hash[:hobby][1]
 "coffee"
 
+## Add a hash value
 hash[:profession] = 'Grand Master Software Engineer'
+
 hash
 {:name=>"Harrison", :age=>27, :hobby=>["coding", "coffee", "golf", "zelda"], :profession=>"Grand Master Software Engineer"}
+
+## Change a hash value
 hash[:age] = 29
 29
+
 hash
 {:name=>"Harrison", :age=>29, :hobby=>["coding", "coffee", "golf", "zelda"], :profession=>"Grand Master Software Engineer"}
+
+## Delete a hash value
 hash.delete(:age)
 29
+
 hash
 {:name=>"Harrison", :hobby=>["coding", "coffee", "golf", "zelda"], :profession=>"Grand Master Software Engineer"}
+
+### Delete a specific array item from a hash value
 hash[:hobby].delete('golf')
 "golf"
+
 hash
 {:name=>"Harrison", :hobby=>["coding", "coffee", "zelda"], :profession=>"Grand Master Software Engineer"}
 
