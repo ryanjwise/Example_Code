3634be82cb1b7239233d5280d555ff33286ddbf2
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-18 12:49:14 +1100

    added array method handling .rb

---
+++ ruby/error-handling/methods.rb
@@ -0,0 +1,7 @@
+arr = ["bob", "ross", "charlie"]
+
+puts arr[0] # = bob
+puts arr[1] # = ross
+puts arr[2] # = charlie
+# puts arr[3] # = error on compilation
+puts arr[3] && arr[3] # = Will only put the value if it exists
