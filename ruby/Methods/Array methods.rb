38604104742ec87753dbd39e0531427b27f133ba
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-18 12:49:28 +1100

    Added Array methods

---
+++ ruby/Methods/Array methods.rb
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5,6]
+
+arr.length      #retruns length od array
+arr.join        #returns array as one continuous element "123456"
+arr.include?()  #Searches for passed element and returns true or false
+arr.delete()    # deletes any antries with a matcing value 
+arr.delete_at() # Delete entry at passed index
