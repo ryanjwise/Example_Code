f6329b6aff5bdfd4c0e44eb3a2ec889c52e2689b
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-23 14:51:09 +1100

    Up;date to match dans code

--- ruby/ca-intro/hangman-game/lib/game-class.rb
+++ ruby/ca-intro/hangman-game/lib/game-class.rb
@@ -5,19 +5,27 @@ class Game
         @word = @words.sample
         @lives = @word.length + 2
         @guessed_letters = []
+        @game_state = true
     end
 
     def run
-        loop do 
+        while @game_state
+            system 'clear'
+            line_seperator
             display_word
             display_lives
+            display_guessed_letters
+            line_seperator
             display_instructions
             process_input(user_input)
         end
     end
 
+    def line_seperator
+        puts "<>"* 10
+    end
+
     def display_word
-        puts
         @word.chars do | char |
             if @guessed_letters.include?(char)
                 print "#{char.upcase} "
@@ -29,7 +37,6 @@ class Game
     end
 
     def display_lives
-        puts
         puts "You have #{@lives} lives remaining"
     end
 
@@ -52,13 +59,40 @@ class Game
     def process_letter(letter)
         @guessed_letters << letter
         update_lives(letter)
+        check_lives
     end
 
     def process_guess(guess)
-
+        if @word == guess
+            win
+        else
+            game_over
+        end
     end
 
     def update_lives(letter)
         @lives -= 1 unless @word.include?(letter)
     end
-end
+
+    def check_lives
+        return unless @lives <= 0 # Guard clause, prevents ecectution of statement if condition not met
+        game_over
+    end
+    def game_over
+            puts "Game Over!"
+            @game_state = false 
+        end
+    end
+
+    def win
+        puts "You win!"
+        @game_state = false
+    end
+
+    def display_guessed_letters
+        puts "Guessed Letters"
+        @guessed_letters.each do |letter|
+            print "#{letter.upcase} "
+        end
+        puts
+    end
