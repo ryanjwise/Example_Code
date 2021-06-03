ab04d1ff1281954ab5d77d26fb06620b62519bdc
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-03 16:35:37 +1000

    Add logic and ternary examples

---
+++ javascript/logical-statments.js
@@ -0,0 +1,7 @@
+function canUserEnter(age) {
+  if (age >= 18 && age < 100) {
+      return true
+  } else {
+      return false
+  }
+}
---
+++ javascript/ternary.js
@@ -0,0 +1,5 @@
+function doesItBlend(boolean) {
+  console.log("Will it blend?")
+  console.log("Lets find out!")
+  return (boolean == true) ? "IT BLENDS!" : "IT DOESN'T BLEND!!";
+}
